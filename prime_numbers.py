f0c69e01dcd05ab03daa6ed36606c1db3c8abff9
RAVIKIRAN NS <140703924+RAVIKIRAN-NS@users.noreply.github.com>
2023-12-08 19:06:35 +0530

    Add files via upload

---
+++ prime_numbers.py
@@ -0,0 +1,8 @@
+num = [prime for prime in range(1,101)]
+print(len(num))
+for n in range(2,len(num)+1):
+    for i in range(2,n):
+        if n % i == 0:
+            break
+    else:
+        print(n)
